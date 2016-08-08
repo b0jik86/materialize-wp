5b9c2ced5e479c277b16991aa4caa173bfe05306
Vladimir Shultsev <Vladimir Shultsev>
2016-08-08 14:23:03 +0300

    files

---
+++ grunt-settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+    sassDir:          'scss/',
+    sassMainFileName: 'style',
+    cssDir:           'css/',
+    cssMainFileDir:   './',
+    cssMainFileName:  'style',
+    jsDir:            'js/',
+    imgDir:           'images/',
+    imgSourceDir:     'sourceimages/',
+    hasBower: {includePaths: ['bower_components/materialize/sass']},
+
+    // sftp server
+    sftpServer:       'example.com',
+    sftpPort:         '2121',
+    sftpLogin:        'login',
+    sftpPas:          'password',
+    sftpDestination:  '/pathTo/css'
+  };
